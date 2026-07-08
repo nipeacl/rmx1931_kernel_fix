791919198ade4b73d53073190433e27b4dd1448d
nipeacl <nipeacl@users.noreply.github.com>
2026-07-08 14:13:02 +0530

    Fix: disable stackprotector (not supported by compiler), fix pipe hiding build errors

--- fix_config.py
+++ fix_config.py
@@ -7,23 +7,14 @@ path = sys.argv[1] if len(sys.argv) > 1 else 'out/.config'
 with open(path) as f:
     c = f.read()
 
-fixes = {
-    'CONFIG_FORTIFY_SOURCE': 'n',
-    'CONFIG_PRIMA_WLAN': 'n',
-    'CONFIG_QCA_CLD_WLAN': 'n',
-    'CONFIG_QCA_CLD_WLAN_V2': 'n',
-    'CONFIG_HDCP_2X': 'n',
-}
-for k, v in fixes.items():
-    c = re.sub(k + r'=y', k + '=n', c)
-
-# STACKPROTECTOR: downgrade STRONG to REGULAR
-c = c.replace(
-    'CONFIG_CC_STACKPROTECTOR_STRONG=y',
-    'CONFIG_CC_STACKPROTECTOR_REGULAR=y'
-)
+# Disable problematic features
+for k in ['CONFIG_FORTIFY_SOURCE', 'CONFIG_PRIMA_WLAN',
+          'CONFIG_QCA_CLD_WLAN', 'CONFIG_QCA_CLD_WLAN_V2',
+          'CONFIG_HDCP_2X', 'CONFIG_CC_STACKPROTECTOR']:
+    c = re.sub(k + r'=y', '# ' + k + ' is not set', c)
+    c = re.sub(k + r'=m', '# ' + k + ' is not set', c)
 
 with open(path, 'w') as f:
     f.write(c)
 
-print('Config fixed for GCC 9+')
+print('Config fixed - stackprotector disabled')
