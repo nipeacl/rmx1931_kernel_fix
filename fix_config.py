c9f2a3a7caefdfaa1c641f8402b58846639bfe0e
nipeacl <nipeacl@users.noreply.github.com>
2026-07-08 14:15:19 +0530

    Fix: stackprotector NONE choice instead of =n

--- fix_config.py
+++ fix_config.py
@@ -7,14 +7,22 @@ path = sys.argv[1] if len(sys.argv) > 1 else 'out/.config'
 with open(path) as f:
     c = f.read()
 
-# Disable problematic features
+# Disable problematic features (set to =n)
 for k in ['CONFIG_FORTIFY_SOURCE', 'CONFIG_PRIMA_WLAN',
           'CONFIG_QCA_CLD_WLAN', 'CONFIG_QCA_CLD_WLAN_V2',
-          'CONFIG_HDCP_2X', 'CONFIG_CC_STACKPROTECTOR']:
+          'CONFIG_HDCP_2X']:
     c = re.sub(k + r'=y', '# ' + k + ' is not set', c)
     c = re.sub(k + r'=m', '# ' + k + ' is not set', c)
+
+# Stackprotector: disable via NONE choice (not via =n which doesn't work)
+c = c.replace('CONFIG_CC_STACKPROTECTOR_STRONG=y', '# CONFIG_CC_STACKPROTECTOR_STRONG is not set')
+c = c.replace('CONFIG_CC_STACKPROTECTOR_REGULAR=y', '# CONFIG_CC_STACKPROTECTOR_REGULAR is not set')
+# Add NONE if not present
+if 'CONFIG_CC_STACKPROTECTOR_NONE=y' not in c and 'CONFIG_CC_STACKPROTECTOR_NONE' not in c:
+    c = c.replace('# CONFIG_CC_STACKPROTECTOR_STRONG is not set',
+                  'CONFIG_CC_STACKPROTECTOR_NONE=y\n# CONFIG_CC_STACKPROTECTOR_STRONG is not set')
 
 with open(path, 'w') as f:
     f.write(c)
 
-print('Config fixed - stackprotector disabled')
+print('Config fixed for GCC 9+ compatibility')
